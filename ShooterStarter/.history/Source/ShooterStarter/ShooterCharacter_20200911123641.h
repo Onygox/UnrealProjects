e079c9593a5d9b794d17a409ff4f5175fbffaa0a
Lionel Miele-Herndon <miell534@newschool.edu>
2020-09-11 12:38:08 -0400

    music and more animations

---
+++ ShooterStarter/.history/Source/ShooterStarter/ShooterCharacter_20200911123641.h
@@ -0,0 +1,65 @@
+// Copyright Lionel Miele-Herndon 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class AA_Gun;
+
+UCLASS()
+class SHOOTERSTARTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const &DamageEvent, class AController *EventInstigator, AActor* DamageCauser) override;
+
+	UFUNCTION(BlueprintPure)
+	bool IsDead() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercent() const;
+
+	void Shoot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void MoveForward(float AxisValue);
+	void LookUp(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookRight(float AxisValue);
+	
+	//for controller gamepads
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 17.5f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxHealth = 100;
+
+	UPROPERTY(VisibleAnywhere)
+	float CurrentHealth;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AA_Gun> GunClass;
+
+	UPROPERTY()
+	AA_Gun * Gun;
+
+};
